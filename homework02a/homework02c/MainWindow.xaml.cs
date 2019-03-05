9cc3d838b696ac782b547825e282e8c7fc6c0668
3017218198 <lljlljly@tju.edu.cn>
2019-03-05 21:04:53 +0800

    second edition

--- homework02a/homework02c/MainWindow.xaml.cs
+++ homework02a/homework02c/MainWindow.xaml.cs
@@ -25,9 +25,30 @@ namespace homework02c
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        /* args[0] = path of the document
+         * args[1] = command line argument[0]
+         * args[2] = command line argument[1]
+         * the number of command line arguments should be two, or the result would be wrong
+         * the default arguments are "argument01" and "argument02" 
+         */
+        string[] args = Environment.GetCommandLineArgs();
+
+        /* @button "Hello"
+         * create a button "Hello_Click" in mainwindow
+         * if you click this button, messagebox will print "hello world"
+         */
+        private void Hello_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Hello World!");
+        }
+
+        /* @button "HelloWithArgs"
+         * create a button "HelloWithArgs_Click" in mainwindow
+         * if you click this button, messagebox will print "hello world" and the arguments of command line
+         */
+        private void HelloWithArgs_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello World! The input arguments of command line are " + args[1] + args[2]);
         }
     }
 }
